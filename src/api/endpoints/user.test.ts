4e6620d8fa430acfdfcf42116ea7c8f3d38db6a4
Lucky3028 <me@mail.cap-l.cc>
2024-05-26 01:14:54 +0900

    test(endpoint): add GET /user 404 test

--- src/api/endpoints/user.test.ts
+++ src/api/endpoints/user.test.ts
@@ -1,6 +1,7 @@
 import { usersEndpoint } from './users';
 import { env } from 'cloudflare:test';
 
-it('', async () => {
-  await usersEndpoint.request('/user-id', {}, env);
+it('returns 404 when get non-registered user', async () => {
+  const res = await usersEndpoint.request('/user-id', {}, env);
+  expect(res.status).toBe(404);
 });
