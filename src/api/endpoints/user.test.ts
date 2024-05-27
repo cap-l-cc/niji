cb0bf43611dd3003cfc3fc7ba61036f5474a9b3c
Lucky3028 <me@mail.cap-l.cc>
2024-05-27 21:59:29 +0900

    test: add getUser returns 200 test

--- src/api/endpoints/user.test.ts
+++ src/api/endpoints/user.test.ts
@@ -1,14 +1,38 @@
 import { User } from '@/domain/user';
 import { usersEndpoint } from './users';
 import { env } from 'cloudflare:test';
+import { drizzle } from 'drizzle-orm/d1';
+import { usersTableSchema } from '@/infra/dbSchema';
 
 const expectedUserId = User.Id.as('test-user-id');
+const expectedUser = User.as({
+  id: expectedUserId,
+  name: 'test-user-name',
+  googleAccountId: 'test-google-account-id',
+});
 
 const fetchUserEp = async (userId: string, method: 'GET' | 'POST') =>
   usersEndpoint.request(`/${userId}`, { method }, env);
 
 describe('GET /:userId', () => {
   const wrappedGetUser = async (userId: string) => fetchUserEp(userId, 'GET');
+
+  it('returns 200 when get registered user', async () => {
+    await drizzle(env.DB).insert(usersTableSchema).values({
+      userId: expectedUserId,
+      userName: expectedUser.name,
+      googleAccountId: expectedUser.googleAccountId,
+      userProfileIconKey: expectedUser.profileIconUrl,
+    });
+
+    const res = await wrappedGetUser(expectedUserId);
+
+    expect(res.status).toBe(200);
+    expect(await res.json()).toStrictEqual({
+      ok: true,
+      data: { user: expectedUser },
+    });
+  });
 
   it('returns 404 when get non-registered user', async () => {
     const res = await wrappedGetUser(expectedUserId);
