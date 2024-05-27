e95f1c4bc9325db01b3c1e5b8c55f20284557bf4
Lucky3028 <me@mail.cap-l.cc>
2024-05-27 21:38:05 +0900

    test: wrap endpoint test with describe

--- src/api/endpoints/user.test.ts
+++ src/api/endpoints/user.test.ts
@@ -4,26 +4,28 @@ import { env } from 'cloudflare:test';
 
 const expectedUserId = User.Id.as('test-user-id');
 
-it('returns 404 when get non-registered user', async () => {
-  const res = await usersEndpoint.request(`/${expectedUserId}`, {}, env);
+describe('GET /:userId', () => {
+  it('returns 404 when get non-registered user', async () => {
+    const res = await usersEndpoint.request(`/${expectedUserId}`, {}, env);
 
-  expect(res.status).toBe(404);
-  expect(await res.json()).toStrictEqual({
-    ok: false,
-    data: { id: expectedUserId },
-    error: `指定されたIDのユーザは存在しません: ${expectedUserId}`,
+    expect(res.status).toBe(404);
+    expect(await res.json()).toStrictEqual({
+      ok: false,
+      data: { id: expectedUserId },
+      error: `指定されたIDのユーザは存在しません: ${expectedUserId}`,
+    });
   });
-});
 
-it('returns 500 when database has a internal problem', async () => {
-  await env.DB.exec('DROP TABLE users;');
+  it('returns 500 when database has a internal problem', async () => {
+    await env.DB.exec('DROP TABLE users;');
 
-  const res = await usersEndpoint.request(`/${expectedUserId}`, {}, env);
+    const res = await usersEndpoint.request(`/${expectedUserId}`, {}, env);
 
-  expect(res.status).toBe(500);
-  expect(await res.json()).toStrictEqual({
-    ok: false,
-    data: { id: expectedUserId },
-    error: 'DBの処理中にエラーが発生しました',
+    expect(res.status).toBe(500);
+    expect(await res.json()).toStrictEqual({
+      ok: false,
+      data: { id: expectedUserId },
+      error: 'DBの処理中にエラーが発生しました',
+    });
   });
 });
