5cb1223ff743d6da2dcb50105b82a6ee7cc5d4bc
Lucky3028 <me@mail.cap-l.cc>
2024-05-27 21:44:51 +0900

    refactor: use wrapper fn for GET /userId tests

--- src/api/endpoints/user.test.ts
+++ src/api/endpoints/user.test.ts
@@ -4,9 +4,14 @@ import { env } from 'cloudflare:test';
 
 const expectedUserId = User.Id.as('test-user-id');
 
+const fetchUserEp = async (userId: string, method: 'GET' | 'POST') =>
+  usersEndpoint.request(`/${userId}`, { method }, env);
+
 describe('GET /:userId', () => {
+  const wrappedGetUser = async (userId: string) => fetchUserEp(userId, 'GET');
+
   it('returns 404 when get non-registered user', async () => {
-    const res = await usersEndpoint.request(`/${expectedUserId}`, {}, env);
+    const res = await wrappedGetUser(expectedUserId);
 
     expect(res.status).toBe(404);
     expect(await res.json()).toStrictEqual({
@@ -19,7 +24,7 @@ describe('GET /:userId', () => {
   it('returns 500 when database has a internal problem', async () => {
     await env.DB.exec('DROP TABLE users;');
 
-    const res = await usersEndpoint.request(`/${expectedUserId}`, {}, env);
+    const res = await wrappedGetUser(expectedUserId);
 
     expect(res.status).toBe(500);
     expect(await res.json()).toStrictEqual({
